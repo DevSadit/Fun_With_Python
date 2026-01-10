48b9636f1d92a0b2d80985c958b53a4bd1784718
DevSadit <dev.sadit@gmail.com>
2026-01-11 02:18:15 +0600

    assignment 1 completed

---
+++ part_1/assignment_1.py
@@ -0,0 +1,95 @@
+# 1. Q1
+# name = input("your name: ")
+# age = int(input("your age: "))
+
+# print(f"Hello {name}, you are {age} years old!")
+
+
+# 2. Q2 ---> wrong
+# a = int(input("first number: "))
+# b = int(input("second number: "))
+
+# print((a + b), (a - b), (a * b), ((a + b) / 2))
+
+
+# Q2 -----> wright answer
+# a = float(input("First number: "))
+# b = float(input("Second number: "))
+
+# print("Sum:", a + b)
+# print("Difference:", a - b)
+# print("Product:", a * b)
+
+# if b != 0:
+#     print("Quotient:", a / b)
+# else:
+#     print("Quotient: undefined (division by zero)")
+
+
+# Q3
+# x = int(input("number 1: "))
+# y = int(input("number 2: "))
+# z = float(input("number 3: "))
+
+# x_float = float(x)
+# y_float = float(y)
+
+# avg = (x_float + y_float + z) / 3
+# print(avg)
+
+
+# Q4
+# user_num = input("Provide a number (e.g., 45): ")
+
+# as_int = int(user_num)
+# as_float = float(user_num)
+# as_str = str(user_num)
+
+# print(as_int, type(as_int))
+# print(as_float, type(as_float))
+# print(as_str, type(as_str))
+
+
+# Q5
+l = 10 + 3 * 2**2
+# print(l)
+
+
+# Q6
+# a = int(input("a number : "))
+# b = int(input("b number : "))
+
+# a, b = b, a
+# print(f"a = {a} and b = {b}")
+
+
+# Q7
+# celsius_str = input("Temperature in Celsius: ")
+# celsius = float(celsius_str)
+
+# fahrenheit = (celsius * (9/5)) + 32
+# print("Fahrenheit:", fahrenheit)
+
+
+# Q8
+# radius = float(input("radius : "))
+# area = 3.14 * (radius * radius)
+# print(area)
+
+
+# Q9
+# p = float(input("Principal: "))
+# r = float(input("Rate: "))
+# t = float(input("Time: "))
+
+# si = (p * r * t) / 100
+# print("Simple Interest:", si)
+
+
+# Q10
+# num = input("Enter a decimal number: ")
+
+# integer_part, fractional_part = num.split(".")
+
+# print("Integer part:", integer_part)
+# print("Fractional part:", fractional_part)
