cb0694c68b3eb7365679ab0f7c04a454b1120266
DevSadit <dev.sadit@gmail.com>
2026-01-11 00:43:08 +0600

    part 1 completed

--- part_1/one.py
+++ part_1/one.py
@@ -1,8 +1,8 @@
 # print("coda catni, bekar katni..!")
-name = "sadit"
+nam = "sadit"
 age = 20
 word = "ai"
-# print(name, age, word)
+# print(nam, age, word)
 
 
 # data types
@@ -28,9 +28,68 @@ empty = None
 
 a = 5
 b = 4.6
-print(a + b)
+# print(a + b) # converts ans in float by default
 
 
 kola = 10
 dola = float(kola)
-print(type(dola))
+# print(type(dola))
+
+# ------------
+# operators   |
+# ------------
+
+# 1. artithmatic
+m = 4
+n = 3
+# print((m + n), (m * n), (m / n), (m % n), (m**n))
+
+
+# 2. relational
+y = 20
+z = 10
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+
+# 3. assignment
+# =, +=, *=, /=, %=, **
+
+x += 2
+# print(x)
+
+x -= 2
+# print(x)
+
+x *= 4
+# print(x)
+
+x /= 2
+# print(int(x))
+
+x %= 3
+# print(x)
+
+
+# 4. logical
+# operators -> and, or, not
+
+# AND
+# print(x > z and y > x)
+# OR
+# print(x > y or y > z)
+# NOT
+# print(not (x > y))
+
+
+# ------------
+# input      |
+# ------------
+
+name = input("whats your name: ")
+print(name)
