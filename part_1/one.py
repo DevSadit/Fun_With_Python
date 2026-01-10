097c7ac581f627be60a2e09e7cb5c43e8f22d7f7
DevSadit <dev.sadit@gmail.com>
2026-01-10 23:56:49 +0600

    part one half completed

---
+++ part_1/one.py
@@ -0,0 +1,36 @@
+# print("coda catni, bekar katni..!")
+name = "sadit"
+age = 20
+word = "ai"
+# print(name, age, word)
+
+
+# data types
+x = 10
+# print(type(x))
+
+
+pi = 3.14
+# print(type(pi))
+
+
+# print(type(word))
+
+isStudent = False
+# print(type(isStudent))
+
+
+empty = None
+# print(type(empty))
+
+
+# type conversation and type casting
+
+a = 5
+b = 4.6
+print(a + b)
+
+
+kola = 10
+dola = float(kola)
+print(type(dola))
