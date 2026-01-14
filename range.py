bb1f625a0dcc86c2954a8d18a82bba246e8b1e67
DevSadit <dev.sadit@gmail.com>
2026-01-14 23:37:11 +0600

    range learned

---
+++ range.py
@@ -0,0 +1,12 @@
+# single argument
+# for i in range(5):
+# print(i)
+
+# 2 arguments - start, stop
+# for j in range(1, 6):
+#     print(j)
+
+
+# 3 arguments - start, stop, step
+# for d in range(10):
+#     print(d)
