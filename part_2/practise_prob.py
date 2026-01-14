06b371a224de085c22fd375e9385313ad98cb72f
DevSadit <dev.sadit@gmail.com>
2026-01-14 23:57:52 +0600

    practise prob solved

---
+++ part_2/practise_prob.py
@@ -0,0 +1,15 @@
+# Print multiplication table for any number
+# num = int(input("enter a number: "))
+# i = 1
+# while i <= 10:
+#     print(i * num)
+#     i += 1
+
+
+# sum of first n using for
+num = int(input("enter a number: "))
+total = 0
+for n in range(num):
+    total += n
+
+print(total)
